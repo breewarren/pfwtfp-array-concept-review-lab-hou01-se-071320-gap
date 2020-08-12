b331f8345ffc8f5af6a3fff7de2746549d66e179
breewarren <ambreea.warren@gmail.com>
2020-08-12 11:19:57 -0500

    Done.

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,12 +1,23 @@
-#Write your code here!
+require 'pry'
+
 def using_max(my_array)
+    my_array.max_by do |element|
+        element
+    end
 end
 
 def find_nth_element(my_array)
+    my_array.find do |element|
+        if element[1][1] == "Nemo"
+            return element[1][1]
+        end
+    end
 end
 
 def flatten_nested_arrays(my_array)
+    my_array.flatten
 end
 
-def flatten_then_sort(my_array)
-end
+    def flatten_then_sort(my_array)
+        my_array.flatten.sort
+    end
